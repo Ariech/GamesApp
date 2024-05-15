9af4cdc560169277c0f2dcf184c875f772bb3589
Dominik <dominikant99@gmail.com>
2024-05-15 14:11:51 +0200

    Add exemplary connection to RAWG API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((d) => setData(d))
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      {data.results ? (
+        data.results.map((result) => <div key={result.id}>{result.name}</div>)
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
 }
 
 export default App;
